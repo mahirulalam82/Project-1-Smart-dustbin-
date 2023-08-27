867241068cd8c0fac2285b9231c904b44a26cfaf
md. mahir ul alam <140166039+mahirulalam82@users.noreply.github.com>
2023-08-28 01:07:24 +0600

    Add files via upload

---
+++ Project 1(smart dustbin).ino
@@ -0,0 +1,34 @@
+
+#include<Servo.h>
+int distance_function(int echo,int trig);
+Servo myservo;
+int echo=5,trig=4;
+void setup()
+{
+Serial.begin(9600);
+ myservo.attach(8); 
+  pinMode(echo,INPUT);
+pinMode(trig,OUTPUT);}
+
+void loop()
+{
+  if(distance_function(echo,trig)<30)
+  {
+   myservo.write(90);
+  delay(2000);
+  }  
+else{
+    myservo.write(0);
+  }
+}
+int distance_function(int echo,int trig){
+  digitalWrite(trig,LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+  
+   int duration=pulseIn(echo,HIGH);
+  int distance=(0.034*duration)/2;
+  return distance;}
